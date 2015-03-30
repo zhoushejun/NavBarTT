68d2877892da9e25eb042ed87bc0c109129b176b
shejunzhou <965678322@qq.com>
2015-03-30 22:53:33 +0800

    Initial Commit

---
+++ NavBarTT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavBarTT
+//
+//  Created by shejun.zhou on 15/3/30.
+//  Copyright (c) 2015年 YiBan.iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavBarTT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NavBarTT
+//
+//  Created by shejun.zhou on 15/3/30.
+//  Copyright (c) 2015年 YiBan.iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
